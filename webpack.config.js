bec974a7a347c914febc21a35f13727637c5fd3f
Aaron Ahn <aaron@countable.ca>
2021-06-28 16:33:25 -0700

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,23 @@ module.exports = {
       filename: 'cortico-min.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    mode: 'production'
+    mode: 'production',
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { 
+                      "targets": "> 0.001%, not dead" 
+                    }]
+                ]
+              }
+            }
+          }
+        ]
+      }
 };
