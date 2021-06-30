b956ad603eeb10992bd5c230e9184da48a2230d2
Aaron Ahn <aaron@countable.ca>
2021-06-29 20:22:51 -0700

    toggleable appointment menu

--- modules/Appointments/AppointmentMenu.js
+++ modules/Appointments/AppointmentMenu.js
@@ -5,13 +5,26 @@ import "./AppointmentMenu.css";
 export function addAppointmentMenu() {
   const appointments = getAppointments();
   appointments.map((appt) => {
-    appt.appendChild(Ellipsis());
+    appt.appendChild(appointmentMenu());
   });
   console.log("Appointments", appointments);
 }
 
 export function appointmentMenu() {
   const container = document.createElement("div");
+  container.classList.add("appointment-menu-container");
+
+  const menuIcon = Ellipsis();
+  container.appendChild(menuIcon);
+
   const menu = document.createElement("div");
+  container.appendChild(menu);
   menu.classList.add("appointment-menu");
+
+  menuIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+    menu.classList.toggle("show");
+  });
+
+  return container;
 }
