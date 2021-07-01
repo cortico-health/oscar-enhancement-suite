03ce537c4137c6ef2ff3943e7c5a83fec379fdd1
Aaron Ahn <aaron@countable.ca>
2021-07-01 12:35:38 -0700

    try waiting for element

--- modules/Utils/Utils.js
+++ modules/Utils/Utils.js
@@ -13,3 +13,23 @@ export function debounce(func, wait, immediate) {
     if (callNow) func.apply(context, args);
   };
 }
+
+export function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
