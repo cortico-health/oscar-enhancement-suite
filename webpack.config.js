7c21db99fe7b7516885f3e9dc4e9a14586a9a792
Aaron Ahn <aaron@countable.ca>
2021-07-20 14:18:32 -0700

    production env

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
     filename: "cortico-min.js",
     path: path.resolve(__dirname, "dist"),
   },
-  devtool: "source-map",
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {
