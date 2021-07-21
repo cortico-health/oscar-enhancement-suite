e74c56d4aeb269d2294ac006bd0983e19b8ba840
Aaron Ahn <aaron@countable.ca>
2021-07-21 08:52:47 -0700

    dockerize and add live reload

---
+++ webpack.config.dev.js
@@ -0,0 +1,79 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./cortico.js",
+  output: {
+    filename: "cortico-min.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 600,
+  },
+  devtool: "source-map",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      browsers: "> 0.001%, not dead",
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: "> 0.25%, not dead, IE > 11",
+                },
+              ],
+            ],
+            assumptions: {
+              setPublicClassFields: true,
+            },
+            plugins: [
+              [
+                "@babel/plugin-proposal-decorators",
+                { decoratorsBeforeExport: true },
+              ],
+              "@babel/plugin-transform-runtime",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              publicPath: "",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
