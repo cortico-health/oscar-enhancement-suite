094fb2169f1aaf457f1353ebea96595996f69599
Aaron Ahn <aaron@countable.ca>
2022-03-29 18:26:29 -0700

    version bump

--- modules/cortico/Widget/WidgetSidebar.js
+++ modules/cortico/Widget/WidgetSidebar.js
@@ -44,7 +44,7 @@ export default function WidgetSidebar({ onClick, items, ...props }) {
           ))}
         </div>
         <div className="tw-text-center tw-text-white tw-text-sm tw-font-sans tw-font-semibold tw-mt-8">
-          2022.03.23
+          2022.3.29
         </div>
       </div>
     </div>
