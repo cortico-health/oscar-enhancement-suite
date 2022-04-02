c2365c293ba21a2fb372941ae6b50810a46f269e
Aaron Ahn <aaron@countable.ca>
2022-04-02 16:47:15 -0700

    little branding

--- modules/cortico/Widget/WidgetSidebar.js
+++ modules/cortico/Widget/WidgetSidebar.js
@@ -3,12 +3,12 @@ import Logo from "../../../resources/icons/logo.svg";
 
 export default function WidgetSidebar({ onClick, items, ...props }) {
   return (
-    <div className="tw-w-full tw-h-full tw-rounded-l-xl tw-px-6 tw-shadow-right tw-bg-gradient-to-bl tw-from-indigo-700 tw-to-blue-1000 tw-bg-blue-1000 ">
+    <div className="tw-w-full tw-h-full tw-rounded-l-xl tw-px-6 tw-shadow-right tw-bg-gradient-to-bl tw-from-indigo-500 tw-to-blue-1000 tw-bg-blue-1000 ">
       <div className="tw-py-8 tw-flex tw-flex-col tw-justify-between tw-h-full tw-w-full">
         <div>
           <div className="tw-mb-8">
             <img className="tw-w-[140px] tw-mx-auto" src={Logo} alt="Cortico" />
-            <span className="tw-block tw-text-white tw-text-xs tw-text-center tw-mt-2 tw-font-sans tw-font-medium tw-tracking-wider">
+            <span className="tw-block tw-text-white tw-text-xs tw-text-center tw-mt-2 tw-font-sans tw-font-normal tw-tracking-wider">
               Oscar Enhancement Suite
             </span>
           </div>
@@ -43,7 +43,7 @@ export default function WidgetSidebar({ onClick, items, ...props }) {
             </div>
           ))}
         </div>
-        <div className="tw-text-center tw-text-white tw-text-sm tw-font-sans tw-font-semibold tw-mt-8">
+        <div className="tw-text-center tw-text-white tw-text-2xs tw-font-sans tw-font-normal tw-opacity-50 tw-mt-8">
           2022.3.29
         </div>
       </div>
