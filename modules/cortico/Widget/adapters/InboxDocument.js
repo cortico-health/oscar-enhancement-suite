874169809c591d9b2670a18575bc3c648360a7c8
Aaron Ahn <aaron@countable.ca>
2022-04-02 09:56:51 -0700

    inbox document

--- modules/cortico/Widget/adapters/InboxDocument.js
+++ modules/cortico/Widget/adapters/InboxDocument.js
@@ -1,7 +1,48 @@
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 export default function InboxDocument() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    console.log("This is re-rendering unfortunately.");
+    try {
+      setLoading(true);
+      const documentImage = document.querySelector(`img[alt="document"]`);
+      if (!documentImage) {
+        throw Error("Could not get Document Image");
+      }
+
+      const documentAnchor = documentImage.parentNode;
+      if (!documentAnchor) {
+        throw Error("Could not get Document Anchor");
+      }
+
+      const href = documentAnchor.href;
+      if (!href) {
+        throw Error("Could not get Document URL");
+      }
+
+      fetch(href)
+        .then((r) => r.blob())
+        .then((blob) => {
+          return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+            reader.addEventListener("load", (evt) => {
+              resolve(reader.result);
+            });
+            reader.addEventListener("error", (evt) => {
+              reject(evt);
+            });
+            reader.readAsDataURL(blob);
+          });
+        })
+        .then((data) => console.log("Doc Data", data))
+        .catch((error) => {
+          console.error(error);
+        });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 }
