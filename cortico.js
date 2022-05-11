4dbb2c63a88f5808ac6e95647942523ed97d3897
Aaron Ahn <aaron@countable.ca>
2022-05-11 16:47:45 -0700

    Support new juno url

--- cortico.js
+++ cortico.js
@@ -155,7 +155,8 @@ const init_cortico = async function () {
   } else if (
     route.indexOf("/eform/efmformadd_data.jsp") > -1 ||
     route.indexOf("/eform/efmshowform_data.jsp") > -1 ||
-    route.indexOf("/casemgmt/forward.jsp") > -1
+    route.indexOf("/casemgmt/forward.jsp") > -1 ||
+    route.indexOf("/oscarEncounter/IncomingEncounter.do") > -1
   ) {
     //const patient_info = await getPatientInfo();
 
