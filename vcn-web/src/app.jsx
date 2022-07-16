3b8ef45d5a98d91d26b39b41e255d651dc029fab
Justin Carretas <justin@countable.ca>
2022-07-16 14:41:50 +0800

    Added vcn-messaging into the project

---
+++ vcn-web/src/app.jsx
@@ -0,0 +1,20 @@
+import { Logo } from './logo'
+
+export function App() {
+  return (
+    <>
+      <Logo />
+      <p className="text-6xl">Hello Vite + Preact! and tailwind :D</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
