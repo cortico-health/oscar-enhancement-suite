8e51f5d358c3b2035573a65ddb9cffe829c88a6a
Justin Carretas <justin@countable.ca>
2022-07-21 05:05:24 +0800

    Testing page

--- vcn-web/src/app.jsx
+++ vcn-web/src/app.jsx
@@ -1,20 +1,27 @@
-import { Logo } from './logo'
-
 export function App() {
+  const chatSocket = new WebSocket('ws://localhost:8426/chat/1/');
+
+  const sendMessage = (e) => {
+    const messageEl = document.getElementById('chatMessage')
+    const message = messageEl.value
+    chatSocket.send(JSON.stringify({
+      'body': message,
+    }));
+    messageEl.value = ''
+  }
+
+  chatSocket.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    const newMessage = JSON.parse(data.text);
+    document.getElementById('messages').innerHTML += newMessage.body
+  }
+
   return (
     <>
-      <Logo />
-      <p className="text-6xl">Hello Vite + Preact! and tailwind :D</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
+      <input id="chatMessage" type="text" />
+      <button onClick={sendMessage}>Submit</button>
+      <div id="messages">
+      </div>
     </>
   )
 }
