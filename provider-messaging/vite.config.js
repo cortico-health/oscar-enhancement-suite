a92dee1977f203b44d8109c269a0fe3550c0427b
Aaron Ahn <aaron@countable.ca>
2022-07-24 13:50:27 -0700

    Dockerize vcn messaging

--- provider-messaging/vite.config.js
+++ provider-messaging/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), preact()]
-})
+  server: {
+    port: 3500,
+  },
+  plugins: [svgr(), preact()],
+});
