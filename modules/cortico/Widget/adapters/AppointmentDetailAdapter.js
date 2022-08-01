fa114700e1befac81f287099da64932387636d9e
Aaron Ahn <aaron@countable.ca>
2022-07-31 20:43:54 -0700

    Bring over appointment menu coloring schemes

--- modules/cortico/Widget/adapters/AppointmentDetailAdapter.js
+++ modules/cortico/Widget/adapters/AppointmentDetailAdapter.js
@@ -1,8 +1,63 @@
 import { create, isEmpty } from "lodash";
 import { createPortal } from "preact/compat";
 import { useEffect, useState } from "preact/hooks";
-import { getAppointments } from "../../Appointments/Appointments";
+import {
+  extractApptUrl,
+  getAppointmentLink,
+  getAppointments,
+} from "../../Appointments/Appointments";
 import AppointmentDetails from "../features/AppointmentDetails/AppointmentDetails";
+import { getDemographicNo } from "../../../Utils/Utils";
+
+const getAppointmentDemographicNo = (apptTd) => {
+  const apptLink = getAppointmentLink(apptTd);
+  const apptUrl = extractApptUrl(apptLink.attributes.onclick.textContent);
+  const demographicNo = getDemographicNo(apptUrl);
+  return demographicNo;
+};
+
+const getAppointmentCache = () => {
+  return JSON.parse(localStorage.getItem("checkCache") || null);
+};
+
+const getPharmacyCache = () => {
+  const pharmaciesCache = JSON.parse(
+    localStorage.getItem("pharmaciesCache") || '{"demographics":[]}'
+  );
+  return pharmaciesCache;
+};
+
+const isPharmacyCached = (cachedDemographics) => {
+  let demographics = Array.isArray(cachedDemographics)
+    ? cachedDemographics
+    : JSON.parse(cachedDemographics);
+  demographics = demographics
+    .filter((x) => x.hasPharmacy)
+    .map((x) => x.demographicNo);
+
+  return demographics.includes(apptInfoItem.demographic_no);
+};
+
+const getPatientStatus = (apptTd) => {
+  const demographicNo = getAppointmentDemographicNo(apptTd);
+  const cache = getAppointmentCache();
+  if (demographicNo && cache) {
+    return cache[demographicNo] && cache[demographicNo].verified;
+  } else {
+    return "unverified";
+  }
+};
+
+const getAppointmentType = (apptTd) => {
+  const anchor = apptTd.querySelector("a.apptStatus");
+  const appStatus = anchor ? anchor.querySelector("img").title : "";
+
+  if (appStatus) {
+    return appStatus.toLowerCase();
+  } else {
+    return null;
+  }
+};
 
 export default function AppointmentDetailAdapter() {
   const [appointments, setAppointments] = useState([]);
@@ -18,9 +73,16 @@ export default function AppointmentDetailAdapter() {
       <>
         {appointments.map((appointment) => {
           return createPortal(
-            <div className="tw-inline-block tw-relative">
-              <AppointmentDetails apptTd={appointment}></AppointmentDetails>
-            </div>,
+            <>
+              <div className="tw-inline-block tw-relative">
+                <AppointmentDetails
+                  apptTd={appointment}
+                  patientStatus={getPatientStatus(appointment)}
+                  type={getAppointmentType(appointment)}
+                ></AppointmentDetails>
+              </div>
+              <div className="tw-inline-block tw-ml-5">Test</div>
+            </>,
             appointment
           );
         })}
