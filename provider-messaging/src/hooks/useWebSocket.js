20e1ec3f72a612345f5d1b808d8837e9c6e323b6
Your Name <you@example.com>
2022-08-08 20:01:03 -0700

    use correct URLs

--- provider-messaging/src/hooks/useWebSocket.js
+++ provider-messaging/src/hooks/useWebSocket.js
@@ -3,9 +3,7 @@ import React from "react";
 /* TODO: I'll transform this into hooks if I'll fully grasp the behaviour of WebSocket in Preact */
 
 const useWebSocket = (url) => {
-  const socket = new WebSocket(
-    "ws://${import.meta.env.VITE_CEREBRO_URL}/chat/1/"
-  );
+  const socket = new WebSocket(`${import.meta.env.VITE_WEBSOCKET_URL}/chat/1/`);
 
   chatSocket.onmessage = (e) => {
     const data = JSON.parse(e.data);
