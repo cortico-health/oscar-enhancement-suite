2091d5c57ee3743772547197244fff306fa3d053
dimdejesus <ddejesus0320@gmail.com>
2022-08-09 23:12:09 +0800

    Added an animation to the plugin (will improve the style)

--- provider-messaging/src/components/organisms/c-plugin.jsx
+++ provider-messaging/src/components/organisms/c-plugin.jsx
@@ -7,17 +7,14 @@ const CPlugin = ({className, ...props}) => {
   const [expandBanner, setExpandBanner] = useState(false);
   return (
     <div {...props}
-      onMouseEnter={() => setExpandBanner(true)} 
+      onMouseEnter={ () => setExpandBanner(true) }
       onMouseLeave={() => setExpandBanner(false)}
       className={`c-plugin ${className}`}>
-      <ASvg className="my-4 mx-4" src="plug"/>
-      {
-        expandBanner ?
-      <div className='-ml-2 mr-5'>
+      <ASvg className="my-4 mx-4" src="plug" />
+      <div className='c-plugin__content mr-4'>
         <h2 className='text-secondary-500 text-h1'>Install EMR plug-in</h2>
         <p className='text-secondary-500 text-h2'>Send messages & files directly in your EMR</p>
       </div>
-      : null }
     </div>
   )
 }
