480ed1490ca0b6fba4cac7a35e994799e5ae333e
dimdejesus <ddejesus0320@gmail.com>
2022-08-10 22:43:06 +0800

    Fixed the unecessary spacing in plugin button.

--- provider-messaging/src/components/organisms/c-plugin.jsx
+++ provider-messaging/src/components/organisms/c-plugin.jsx
@@ -11,7 +11,7 @@ const CPlugin = ({className, ...props}) => {
       onMouseLeave={() => setExpandBanner(false)}
       className={`c-plugin ${className}`}>
       <ASvg className="my-4 mx-4" src="plug" />
-      <div className='c-plugin__content mr-4'>
+      <div className='c-plugin__content'>
         <h2 className='text-secondary-500 text-h1'>Install EMR plug-in</h2>
         <p className='text-secondary-500 text-h2'>Send messages & files directly in your EMR</p>
       </div>
