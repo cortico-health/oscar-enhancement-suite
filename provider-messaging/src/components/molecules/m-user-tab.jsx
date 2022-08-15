c847cbc78b2b5a5fc72009b0fb4e5fab8b0cd891
dimdejesus <ddejesus0320@gmail.com>
2022-08-15 19:38:12 +0800

    Editing the names in user tab

--- provider-messaging/src/components/molecules/m-user-tab.jsx
+++ provider-messaging/src/components/molecules/m-user-tab.jsx
@@ -9,8 +9,8 @@ const MUserTab = ({ user, onChange, index }) => {
           <MProfilePicture className='o-aspect-ratio__content' avatar={ user.avatar } onClick={ () => alert('profile here') } />
         </div>
         <label for={ 'name' + index } className='tw-cursor-pointer tw-text-left'>
-          <h2 className='text-secondary-500 text-contact1 font-medium'> { user.full_name }</h2>
-          <p className='text-secondary-300 text-select3'> { `${user.title}, ${user.clinic_name}` }</p>
+          <h2 className='text-secondary-500 text-contact1 font-medium'> { user.title && user.title + ". " } { user.full_name }</h2>
+          <p className='text-secondary-300 text-select3'> { user.clinic_name }</p>
         </label>
       </div>
       <input onChange={onChange} index={index} className='w-5 h-5 tw-rounded-md' type="checkbox" id={"name" + index} name="name" value={user.id} />
