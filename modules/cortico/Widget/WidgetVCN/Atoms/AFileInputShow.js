bf5e0edac620eef179a986a17dfeb2f21e1a10ad
dimdejesus <ddejesus0320@gmail.com>
2022-08-28 08:35:52 +0800

    Made the File Input Show functional

--- modules/cortico/Widget/WidgetVCN/Atoms/AFileInputShow.js
+++ modules/cortico/Widget/WidgetVCN/Atoms/AFileInputShow.js
@@ -1,6 +1,12 @@
 import { isAudio,isDocument,isHTML,isImage,isSheet,isTextFile } from '../../../../helper/determine-file-type';
 import ASvg from './ASvg';
+import ExitLogo from "../../../../../resources/icons/exit.svg";
 
+import MusicLogo from "../../../../../resources/icons/music.svg";
+import DocumentLogo from "../../../../../resources/icons/document.svg";
+import DocumentTextLogo from "../../../../../resources/icons/document-text.svg";
+import TableLogo from "../../../../../resources/icons/table.svg";
+import CodeLogo from "../../../../../resources/icons/code.svg";
 
 const AFileInputShow = ({ fileInput,exit }) => {
 
@@ -9,13 +15,13 @@ const AFileInputShow = ({ fileInput,exit }) => {
     return (
         <div className="tw-flex tw-items-center tw-gap-4">
             { isImage(fileInput.type) && <img width="50" src={ fileInput.dataURL } /> }
-            { isAudio(fileInput.type) && <ASvg src="music" className="tw-h-16 tw-w-16" /> }
-            { isDocument(fileInput.type) && <ASvg src="document" className="tw-h-16 tw-w-16" /> }
-            { isTextFile(fileInput.type) && <ASvg src="document-text" className="tw-h-16 tw-w-16" /> }
-            { isSheet(fileInput.type) && <ASvg src="table" className="tw-h-16 tw-w-16" /> }
-            { isHTML(fileInput.type) && <ASvg src="code" className="tw-h-16 tw-w-16" /> }
+            { isAudio(fileInput.type) && <ASvg src={ MusicLogo } className="tw-h-16 tw-w-16" /> }
+            { isDocument(fileInput.type) && <ASvg src={ DocumentLogo } className="tw-h-16 tw-w-16" /> }
+            { isTextFile(fileInput.type) && <ASvg src={ DocumentTextLogo } className="tw-h-16 tw-w-16" /> }
+            { isSheet(fileInput.type) && <ASvg src={ TableLogo } className="tw-h-16 tw-w-16" /> }
+            { isHTML(fileInput.type) && <ASvg src={ CodeLogo } className="tw-h-16 tw-w-16" /> }
             <p>{ fileInput.name }</p>
-            <ASvg src="exit" className="tw-cursor-pointer" onClick={ exit } />
+            <ASvg src={ ExitLogo } className="tw-cursor-pointer" onClick={ exit } />
         </div>
     )
 }
