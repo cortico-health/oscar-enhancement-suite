e1d47db54adef5ed472da1a213c01fbba9b8e3c6
dimdejesus <ddejesus0320@gmail.com>
2022-08-29 10:23:26 +0800

    Proper Styling for text boxes.

--- modules/cortico/Widget/WidgetVCN/Atoms/AInput.js
+++ modules/cortico/Widget/WidgetVCN/Atoms/AInput.js
@@ -3,9 +3,9 @@ import { useRef } from "preact/hooks";
 
 const inputEffects = "placeholder:tw-text-primary-300 focus:tw-outline-primary-600";
 
-const a_input = "tw-text-input tw-w-full tw-my-3 tw-text-secondary-500" + inputEffects;
+const input_class = "tw-text-input tw-w-full tw-my-3 tw-text-secondary-500 tw-outline " + inputEffects;
 
-const textarea_class = "tw-text-input tw-w-full tw-my-3 tw-text-secondary-500 tw-outline tw-outline-1 tw-outline-secondary-100 " + inputEffects;
+const textarea_class = "tw-text-input tw-w-full tw-my-3 tw-text-secondary-500 tw-outline " + inputEffects;
 
 
 const AInput = ({
@@ -23,14 +23,14 @@ const AInput = ({
   const inputRef = useRef(null);
 
   return (
-    <div className={ `${a_input} tw-relative ${className}` } { ...props }>
+    <div className={ `tw-relative ${className}` } { ...props }>
       <div className="tw-absolute tw-w-full tw-h-full tw-pointer-events-none tw-top-0 tw-left-0">
         { children }
       </div>
       <label htmlFor={ name }></label>
       { type == "text" ? (
         <input
-          className={ `${inputClass}` }
+          className={ `${input_class} ${inputClass}` }
           onInput={ onInput }
           onChange={ onChange }
           ref={ inputRef }
--- modules/cortico/Widget/WidgetVCN/Molecules/MSearch.js
+++ modules/cortico/Widget/WidgetVCN/Molecules/MSearch.js
@@ -4,10 +4,9 @@ import ASvg from '../Atoms/ASvg';
 
 import SearchLogo from "../../../../../resources/icons/search.svg"
 
-const MSearch = ({ className,onInput,...props }) => {
-
+const MSearch = ({ className = "",onInput,...props }) => {
   return (
-    <AInput onInput={ onInput } name='search' { ...props } className={ `tw-rounded-l-full tw-rounded-r-full tw-pl-11 tw-py-3 tw-min-w-[140px] ${className ? className : ''}` }>
+    <AInput onInput={ onInput } name='search' { ...props } className={ `${className}` } inputClass="tw-rounded-l-full tw-rounded-r-full tw-pl-11 tw-py-3 tw-w-full">
       <img src={ SearchLogo } className='tw-absolute tw-cursor-pointer tw-pointer-events-auto tw-left-4 tw-top-1/2 -tw-translate-y-1/2' />
     </AInput>
   )
