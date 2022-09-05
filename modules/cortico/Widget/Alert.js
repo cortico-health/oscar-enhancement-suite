2173a65a65e09d4084eb5008bc02061f4bcd1101
dimdejesus <ddejesus0320@gmail.com>
2022-09-06 02:22:11 +0800

    Adding Variants to Alert Module

--- modules/cortico/Widget/Alert.js
+++ modules/cortico/Widget/Alert.js
@@ -1,31 +1,49 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { ExclamationIcon } from "@heroicons/react/solid";
+import { ExclamationIcon,InformationCircleIcon,CheckCircleIcon } from "@heroicons/react/solid";
 import classNames from "classnames";
-export default function Alert({ size, title, message, className, ...props }) {
+
+export default function Alert({ size,title,message,className,children,variant = "warning",...props }) {
   return (
     <div
       className={classNames(
-        "tw-rounded-md tw-bg-yellow-50 tw-p-4 tw-border tw-border-black tw-border-opacity-5",
+        `tw-rounded-md tw-p-4 tw-border tw-border-black tw-border-opacity-5`,
+        variant === "warning" && "tw-bg-yellow-50",
+        variant === "success" && "tw-bg-emerald-50",
+        variant === "info" && "tw-bg-blue-100",
         className
       )}
     >
       <div className="tw-flex">
         <div className="tw-flex-shrink-0">
-          <ExclamationIcon
-            className="tw-h-5 tw-w-5 tw-text-yellow-400"
+          { variant === "warning" && <ExclamationIcon
+            className={ `tw-h-5 tw-w-5 tw-text-yellow-400` }
             aria-hidden="tw-true"
-          />
+          /> }
+          { variant === "success" && <CheckCircleIcon
+            className={ `tw-h-5 tw-w-5 tw-text-emerald-400` }
+            aria-hidden="tw-true"
+          /> }
+          { variant === "info" && <InformationCircleIcon
+            className={ `tw-h-5 tw-w-5 tw-text-blue-400` }
+            aria-hidden="tw-true"
+          /> }
         </div>
         <div className="tw-ml-3">
           <h3
             className={classNames(
-              "tw-m-0 tw-p-0 tw-font-medium tw-text-yellow-800",
+              `tw-m-0 tw-p-0 tw-font-medium`,
+              variant === "warning" && "tw-text-yellow-800",
+              variant === "success" && "tw-text-emerald-800",
+              variant === "info" && "tw-text-blue-800",
               size === "sm" ? "tw-text-xs" : "tw-text-sm"
             )}
           >
             {title}
           </h3>
-          <div className={classNames("tw-mt-2 tw-text-sm tw-text-yellow-700")}>
+          <div className={ classNames(`tw-mt-2 tw-text-sm`,
+            variant === "warning" && "tw-text-yellow-700",
+            variant === "success" && "tw-text-emerald-700",
+            variant === "info" && "tw-text-blue-700") }>
             <p
               className={classNames(
                 size === "sm" ? "tw-text-xs" : "tw-text-sm"
@@ -33,6 +51,7 @@ export default function Alert({ size, title, message, className, ...props }) {
             >
               {message}
             </p>
+            { children || null }
           </div>
         </div>
       </div>
