6acff2fa87d0fda74ec40b5e8e928ce2b51d9f1f
Justin Carretas <justin@countable.ca>
2022-09-06 08:26:54 +0800

    Remove dead code

--- modules/cortico/Widget/WidgetVCN/Pages/PIndex.js
+++ modules/cortico/Widget/WidgetVCN/Pages/PIndex.js
@@ -1,32 +1,17 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect } from "preact/hooks";
 import { useStore } from "../../store/mobx";
 import CMessageList from '../Organisms/CMessageList'
 import Sidebar from '../Sidebar'
 import { observer } from "mobx-react-lite";
-import { getWsChatUrl } from "../../../../Utils/VcnUtils";
 
 const PIndex = () => {
-  const { patientStore, conversationStore } = useStore();
-  /* const router = useRouter()[0]; */
-
-  const getMostRecentConversation = () => {
-    return _.first(conversationStore.conversations)
-  }
+  const { conversationStore } = useStore();
 
   useEffect(() => {
     return () => {
       conversationStore.conversations.selected = null;
     }
   }, [])
-
-  useEffect(() => {
-    // Justin: Disable this behavior for now
-    // Reason: It auto selects patient on conversation select, so user cannot see all conversations
-    // if (!_.has(router.matches, 'id')) {
-    //   const mostRecentConversation = getMostRecentConversation()
-    //   if (mostRecentConversation) route(`/chat/${mostRecentConversation.id}`)
-    // }
-  }, [conversationStore.conversations.all]);
 
   return (
     <div id="upload-confirm" className="tw-relative tw-flex tw-w-full tw-h-full">
