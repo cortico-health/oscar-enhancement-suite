d6fd29f473c83f87bad1233947c54ce308649ae0
Your Name <you@example.com>
2022-09-06 15:41:19 -0700

    auto version

--- pkg.bash
+++ pkg.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 export NODE_ENV=production
+VERSION=$(date +"%Y.%m.%d")
+
+perl -i.bak -p -e "s/__PLUGIN_VERSION__/${VERSION}/g" manifest.chrome.json
+perl -i.bak -p -e "s/__PLUGIN_VERSION__/${VERSION}/g" manifest.firefox.json
+perl -i.bak -p -e "s/__PLUGIN_VERSION__/${VERSION}/g" cortico.js
+perl -i.bak -p -e "s/__PLUGIN_VERSION__/${VERSION}/g" modules/cortico/Widget/WidgetSidebar.js
 
 yarn
 yarn build
@@ -16,3 +22,7 @@ zip -r cortico-chrome.zip dist resources manifest.json
 
 zip -r oscar-enhancement-suite.zip . -x '*.git*' -x '*.zip' -x '*node_modules*'
 
+cp manifest.chrome.json.bak manifest.chrome.json
+cp manifest.firefox.json.bak manifest.firefox.json
+cp cortico.js.bak cortico.js
+cp modules/cortico/Widget/WidgetSidebar.js.bak modules/cortico/Widget/WidgetSidebar.js
