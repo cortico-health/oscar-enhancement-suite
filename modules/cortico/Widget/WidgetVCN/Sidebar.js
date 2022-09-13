340c775f426816973fc4b1644f576744194dc029
Justin Carretas <justin@countable.ca>
2022-09-13 19:32:30 +0800

    Fix conversations tab scroll bug

--- modules/cortico/Widget/WidgetVCN/Sidebar.js
+++ modules/cortico/Widget/WidgetVCN/Sidebar.js
@@ -15,34 +15,27 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="tw-flex tw-w-[550px]">
-      <div className="tw-bg-gray-50 tw-shadow tw-h-full tw-w-full tw-block">
-        <PatientCard
-          className="tw-mt-6 tw-pb-6 tw-px-6 tw-h-min tw-border-b tw-border-gray-200"
-        />
-        {
-          <>
-            <div className="tw-flex tw-mt-4 tw-mx-5 tw-justify-between tw-items-center">
-              <h2 className="tw-text-secondary-500 tw-font-bold tw-text-md">
-                {
-                  patientStore?.patients?.selected ? <>
-                    Conversations on
-                    <span className="text-primary-500">
-                      {" " + patientStore?.patients?.selected.first_name
-                        + " " + patientStore?.patients?.selected.last_name}
-                    </span>
-                  </>
-                    : "All conversations"
-                }
-              </h2>
+    <div className="tw-flex tw-flex-col tw-w-[550px] tw-bg-gray-50 tw-shadow tw-h-full">
+      <PatientCard
+        className="tw-mt-6 tw-pb-6 tw-px-6 tw-h-min tw-border-b tw-border-gray-200"
+      />
+      <div className=" tw-flex-none tw-flex tw-mt-4 tw-mx-5 tw-justify-between tw-items-center">
+        <h2 className="tw-text-secondary-500 tw-font-bold tw-text-md">
+          {
+            patientStore?.patients?.selected ? <>
+              Conversations on
+              <span className="text-primary-500">
+                {" " + patientStore?.patients?.selected.first_name
+                  + " " + patientStore?.patients?.selected.last_name}
+              </span>
+            </>
+              : "All conversations"
+          }
+        </h2>
 
-              <img className="tw-cursor-pointer" src={AddLogo} alt="Cortico" onClick={handleClick} />
-            </div>
-
-            <CConversationList />
-          </>
-        }
+        <img className="tw-cursor-pointer" src={AddLogo} alt="Cortico" onClick={handleClick} />
       </div>
+      <CConversationList />
     </div>
   )
 }
