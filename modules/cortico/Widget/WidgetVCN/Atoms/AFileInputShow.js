8b90bb1be87fedccaa75b9be14c444db460d68e6
Justin Carretas <justin@countable.ca>
2022-09-16 10:15:09 +0800

    Fixed file upload preview

--- modules/cortico/Widget/WidgetVCN/Atoms/AFileInputShow.js
+++ modules/cortico/Widget/WidgetVCN/Atoms/AFileInputShow.js
@@ -1,4 +1,4 @@
-import { isAudio,isDocument,isHTML,isImage,isSheet,isTextFile } from '../../../../helper/determine-file-type';
+import { isAudio, isDocument, isHTML, isImage, isSheet, isTextFile } from '../../../../helper/determine-file-type';
 import ASvg from './ASvg';
 import ExitLogo from "../../../../../resources/icons/exit.svg";
 
@@ -9,25 +9,25 @@ import TableLogo from "../../../../../resources/icons/table.svg";
 import CodeLogo from "../../../../../resources/icons/code.svg";
 
 
-const AFileInputShow = ({ fileInput,exit }) => {
+const AFileInputShow = ({ fileInput, exit }) => {
 
     if (!fileInput) return null;
 
     const dataURL = fileInput.data;
     const fileName = fileInput.name;
-    const type = file.extension;
+    const type = fileInput.extension;
 
     return (
         <div className="tw-flex tw-p-1 tw-mr-1 tw-border tw-rounded-sm tw-items-center tw-gap-4 tw-bg-white">
-            {/* TODO: Dwight - Convert this into tailwind */ }
-            { isImage(type) && <img style="width: 40px!important; height: 40px!important; object-fit: cover;" src={ dataURL } /> }
-            { isAudio(type) && <ASvg src={ MusicLogo } className="tw-h-9 tw-w-9" /> }
-            { isDocument(type) && <ASvg src={ DocumentLogo } className="tw-h-9 tw-w-9" /> }
-            { isTextFile(type) && <ASvg src={ DocumentTextLogo } className="tw-h-9 tw-w-9" /> }
-            { isSheet(type) && <ASvg src={ TableLogo } className="tw-h-9 tw-w-9" /> }
-            { isHTML(type) && <ASvg src={ CodeLogo } className="tw-h-9 tw-w-9" /> }
-            { !isImage(type) && <p className="tw-text-sm">{ fileName }</p> }
-            <ASvg src={ ExitLogo } className="tw-cursor-pointer" onClick={ exit } />
+            {/* TODO: Dwight - Convert this into tailwind */}
+            {isImage(type) && <img style="width: 40px!important; height: 40px!important; object-fit: cover;" src={dataURL} />}
+            {isAudio(type) && <ASvg src={MusicLogo} className="tw-h-9 tw-w-9" />}
+            {isDocument(type) && <ASvg src={DocumentLogo} className="tw-h-9 tw-w-9" />}
+            {isTextFile(type) && <ASvg src={DocumentTextLogo} className="tw-h-9 tw-w-9" />}
+            {isSheet(type) && <ASvg src={TableLogo} className="tw-h-9 tw-w-9" />}
+            {isHTML(type) && <ASvg src={CodeLogo} className="tw-h-9 tw-w-9" />}
+            {!isImage(type) && <p className="tw-text-sm">{fileName}</p>}
+            <ASvg src={ExitLogo} className="tw-cursor-pointer" onClick={exit} />
         </div>
     )
 }
