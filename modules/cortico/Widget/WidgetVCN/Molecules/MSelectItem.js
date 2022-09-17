435a5002bf321f60973bbed9450603e66ec0834d
dimdejesus <ddejesus0320@gmail.com>
2022-09-17 18:05:47 +0800

    Hover states on Select Patient Page.

--- modules/cortico/Widget/WidgetVCN/Molecules/MSelectItem.js
+++ modules/cortico/Widget/WidgetVCN/Molecules/MSelectItem.js
@@ -1,11 +1,11 @@
 const MSelectItem = ({ className,patient = null,selected = false,...props }) => {
 
     return (
-        <tr { ...props } className={ ` ${selected ? 'tw-bg-primary-500' : ''} tw-cursor-pointer` }>
-            <td className='tw-rounded-l-lg tw-py-5 tw-pl-7'> <p className={ `${selected ? 'tw-text-white' : 'tw-text-secondary-500'} tw-text-select1 tw-font-medium` }>{ patient.first_name + " " + patient.last_name }</p></td>
-            <td className='tw-px-6'><p className={ `${selected ? 'tw-text-white' : 'tw-text-secondary-200'} tw-text-select2 tw-font-medium` }>{ new Date(patient.birth_date).toLocaleDateString() }</p></td>
-            <td className='tw-px-6'><p className={ `${selected ? 'tw-text-white' : 'tw-text-secondary-500'} tw-text-select2 tw-font-medium` }>{ patient.hin }</p></td>
-            <td className='tw-rounded-r-lg tw-py-5 tw-pr-7'><a className={ `${selected ? 'tw-text-white' : 'tw-text-primary-500'} tw-text-select2 tw-font-medium` } href={ `mailto:${patient.email}` }>{ patient.email }</a></td>
+        <tr { ...props } className={ ` ${selected ? 'tw-bg-primary-500' : ''}` }>
+            <td className='tw-rounded-l-lg tw-py-5 tw-pl-7 tw-cursor-pointer'> <p className={ `${selected ? 'tw-text-white' : 'tw-text-secondary-500'} tw-text-select1 tw-font-medium` }>{ patient.first_name + " " + patient.last_name }</p></td>
+            <td className='tw-px-6 tw-cursor-pointer'><p className={ `${selected ? 'tw-text-white' : 'tw-text-secondary-200'} tw-text-select2 tw-font-medium tw-cursor-pointer` }>{ new Date(patient.birth_date).toLocaleDateString() }</p></td>
+            <td className='tw-px-6 tw-cursor-pointer'><p className={ `${selected ? 'tw-text-white' : 'tw-text-secondary-500'} tw-text-select2 tw-font-medium tw-cursor-pointer` }>{ patient.hin }</p></td>
+            <td className='tw-rounded-r-lg tw-py-5 tw-pr-7 tw-cursor-pointer'><a className={ `${selected ? 'tw-text-white' : 'tw-text-primary-500'} tw-text-select2 tw-font-medium` } href={ `mailto:${patient.email}` }>{ patient.email }</a></td>
         </tr>
     )
 }
