61cc03a3b8a175d71812ad2853798fb4080dcca3
dimdejesus <ddejesus0320@gmail.com>
2022-09-17 18:21:11 +0800

    Loading state in Patient Select Page.

--- modules/cortico/Widget/WidgetVCN/Organisms/CRecentList.js
+++ modules/cortico/Widget/WidgetVCN/Organisms/CRecentList.js
@@ -1,31 +1,36 @@
 
-/* import { useStore } from '../../state'; */
 import MRecentItem from "../Molecules/MRecentItem";
 import { useDispatch } from 'react-redux';
-import { patientsData } from "../../../../../resources/js/data";
 import { useStore } from "../../store/mobx";
-/* import { observer } from 'mobx-react-lite'; */
+import ASpinner from "../Atoms/ASpinner";
 
 const CRecentList = ({ ...props }) => {
 
-    const { patientStore, conversationStore } = useStore();
+    const { patientStore } = useStore();
 
     const dispatch = useDispatch();
 
     return (
-        <div className='tw-px-5 tw-pt-6' {...props}>
-            <h2 className='tw-text-secondary-500 tw-font-bold tw-text-h1'>Recent patients</h2>
-            {
-                patientStore?.patients?.all?.map(patient => {
-                    return <MRecentItem onClick={() => {
-                        patientStore.selectPatient(patient)
-                        dispatch({
-                            type: "sidebar/setCurrent",
-                            payload: "Provider Messaging",
-                        });
-                    }}
-                        patient={patient} />
-                })
+        <div className="tw-bg-gray-50 tw-shadow tw-h-full tw-block tw-w-[550px]">
+            { patientStore.patients.all ?
+                <div className='tw-px-5 tw-pt-6' { ...props }>
+                    <h2 className='tw-text-secondary-500 tw-font-bold tw-text-h1'>Recent patients</h2>
+                    {
+                        patientStore.patients.all?.map(patient => {
+                            return <MRecentItem onClick={ () => {
+                                patientStore.selectPatient(patient);
+                                dispatch({
+                                    type: "sidebar/setCurrent",
+                                    payload: "Provider Messaging",
+                                });
+                            } } patient={ patient } />
+                        })
+                    }
+                </div>
+                :
+                <div className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-2 tw-w-full tw-h-full">
+                    <ASpinner variant="md" />
+                </div>
             }
         </div>
     )
