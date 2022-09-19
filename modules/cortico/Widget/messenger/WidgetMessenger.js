f160fd15f6586b56645af66ef304f5d82c990544
dimdejesus <ddejesus0320@gmail.com>
2022-09-19 21:46:13 +0800

    Modularized Login Components.

--- modules/cortico/Widget/messenger/WidgetMessenger.js
+++ modules/cortico/Widget/messenger/WidgetMessenger.js
@@ -1,11 +1,10 @@
 import MessengerWindow from "../../../Messenger/MessengerWindow";
 import FeatureDetector from "../adapters/FeatureDetecter";
-import SignIn from "../../../../resources/illustrations/undraw_signin.svg";
 import { useSelector, useDispatch } from "react-redux";
-import Button, { PrimaryButton } from "../../../core/Button";
 import NotAvailable from "../base/NotAvailable";
 import Header from "../base/Header";
 import LabResultsAdapter from "../adapters/LabResultsAdapter";
+import RequiresLogin from "../RequiresLogin";
 
 export default function WidgetMessenger() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
@@ -46,37 +45,5 @@ export default function WidgetMessenger() {
         );
       }}
     </FeatureDetector>
-  );
-}
-
-function RequiresLogin() {
-  const dispatch = useDispatch();
-
-  const handleSignIn = () => {
-    dispatch({
-      type: "sidebar/setCurrent",
-      payload: "Account",
-    });
-  };
-
-  return (
-    <div className="tw-font-sans">
-      <img
-        src={SignIn}
-        alt="Sign In"
-        className="tw-block tw-w-[125px] tw-mx-auto"
-      />
-      <div>
-        <p className="tw-max-w-[325px] tw-mx-auto tw-mt-6 tw-text-gray-700 tw-text-sm tw-text-center">
-          This feature requires you to{" "}
-          <span className="tw-font-semibold">sign in</span>{" "}
-        </p>
-        <div className="tw-flex tw-justify-center tw-mt-4">
-          <PrimaryButton size="sm" onClick={handleSignIn}>
-            Sign In
-          </PrimaryButton>
-        </div>
-      </div>
-    </div>
   );
 }
